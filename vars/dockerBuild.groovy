b50c437b4b29cf2bb557a4d940d46ae7e9a17168
maroof <maroofshaikh09@gmail.com>
2023-07-05 13:23:39 +0530

    added docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,10 @@
 def call(String project, String ImageTag, String hubUser) {
     def imageName = "${hubUser}/${project}"
     
-    sh  """
-     sudo chmod 777 /var/run/docker.sock
-     docker build -t imageName Dockerfile .
-     docker image tag  imageName:${ImageTag}
-     docker image tag imageName imageName:latest
-        """
+    sh """
+    sudo chmod 777 /var/run/docker.sock
+    docker build -t ${imageName} Dockerfile .
+    docker image tag ${imageName}:${ImageTag}
+    docker image tag ${imageName} ${imageName}:latest
+    """
 }
