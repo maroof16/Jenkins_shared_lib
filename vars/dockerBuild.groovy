60d04a147e3f40d08eb3036e01398657f8eb10a1
maroof <maroofshaikh09@gmail.com>
2023-07-05 21:42:30 +0530

    added docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,15 +1,15 @@
-def call( String javaApp, String Dockerfile , String dockerRegistry) {
+//def call( String javaApp, String Dockerfile , String dockerRegistry) {
     //def imageName = "${hubUser}/${project}"
-    
+def call(String project, String ImageTag, String hubUser){  
     // sh """
     // sudo chmod 777 /var/run/docker.sock
     // docker build -t ${hubUser}/${project} .
     // docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
     // docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     // """
-    docker.withRegistry(dockerRegistry) {
-              def dockerImage = docker.build(javaApp, "-f ${Dockerfile} .")
-              dockerImage.push()
-            }
+    // docker.withRegistry(dockerRegistry) {
+    //           def dockerImage = docker.build(javaApp, "-f ${Dockerfile} .")
+    //           //dockerImage.push()
+    //         }
 
 }
