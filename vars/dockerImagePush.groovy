edd2b8eaee68d7246889df69739a4dd5981b2a36
maroof <maroofshaikh09@gmail.com>
2023-07-06 13:02:48 +0530

    added docker image push to dockerhub

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,11 +1,9 @@
 def call(String ImageName, String ImageTag, String DockerHubUser){  
     withCredentials([usernamePassword(
         credentialsId: 'docker-credentials', 
-        passwordVariable: 'docker-pass', 
-        usernameVariable: 'docker-user'
+        passwordVariable: 'docker_pass', 
+        usernameVariable: 'docker_user'
     )]) {
-        def docker_pass = env.docker-pass
-        def docker_user = env.docker-user
         sh " docker login -u '$docker_user' -p '$docker_pass'"
         }
     sh " docker image push ${DockerHubUser}/${ImageName}:${ImageTag} "
