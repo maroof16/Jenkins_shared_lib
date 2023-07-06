06af2461ddfa3d2f5ff2aec77992251f4f3a94c9
maroof <maroofshaikh09@gmail.com>
2023-07-06 11:40:09 +0530

    added docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -8,6 +8,5 @@ def call(String ImageName, String ImageTag, String DockerHubUser){
      docker build -t ${DockerHubUser}/${ImageName} .
      docker image tag ${DockerHubUser}/${ImageName} ${DockerHubUser}/${ImageName}:${ImageTag}
      docker image tag ${DockerHubUser}/${ImageName} ${DockerHubUser}/${ImageName}:latest
-     docker rmi ${DockerHubUser}/${ImageName} ${DockerHubUser}/${ImageName}:latest
   """
 }
