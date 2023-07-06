e7c8d7f8aa5469070b9567adafc67df4de25d302
maroof <maroofshaikh09@gmail.com>
2023-07-06 13:18:07 +0530

    added docker image push to dockerhub

--- vars/dockerdelete.groovy
+++ vars/dockerdelete.groovy
@@ -1,14 +1,21 @@
-def call() {
-    sh """
-        docker_image_details=\$(docker images)
+// def call() {
+//     sh """
+//         docker_image_details=\$(docker images)
 
-        first_line_image_id=\$(echo "\$docker_image_details" | awk 'NR==2 {print \$3}')
-        second_line_image_id=\$(echo "\$docker_image_details" | awk 'NR==3 {print \$3}')
+//         first_line_image_id=\$(echo "\$docker_image_details" | awk 'NR==2 {print \$3}')
+//         second_line_image_id=\$(echo "\$docker_image_details" | awk 'NR==3 {print \$3}')
 
-        echo "First Line Image ID: \$first_line_image_id"
-        echo "Second Line Image ID: \$second_line_image_id"
+//         echo "First Line Image ID: \$first_line_image_id"
+//         echo "Second Line Image ID: \$second_line_image_id"
 
-        echo "Deleting Docker images..."
-        docker rmi -f "\$first_line_image_id" "\$second_line_image_id"
-    """
+//         echo "Deleting Docker images..."
+//         docker rmi -f "\$first_line_image_id" "\$second_line_image_id"
+//     """
+// }
+
+
+
+def call(String ImageName, String ImageTag, String DockerHubUser){
+     sh " docker image rmi ${DockerHubUser}/${ImageName}:${ImageTag} "
+    sh " docker image rmi ${DockerHubUser}/${ImageName}:latest "
 }
