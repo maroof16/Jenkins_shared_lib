32ad9afa96a099dcc337f76546f1f1154291c279
maroof <maroofshaikh09@gmail.com>
2023-07-06 12:18:37 +0530

    added docker build

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(String ImageName, String ImageTag, String DockerHubUser) {
     sh """
         trivy image ${DockerHubUser}/${ImageName}:latest > trivyscan.txt
         cat trivyscan.txt
