1f7615ca69bb84d4fac74bf6c6753c113a209be8
maroof <maroofshaikh09@gmail.com>
2023-07-06 11:47:13 +0530

    added docker build

--- vars/dockerdelete.groovy
+++ vars/dockerdelete.groovy
@@ -1,15 +1,15 @@
  def call(){
-sh """
+     sh """
  
-  docker_image_details=$(docker images)
+        docker_image_details=$(docker images)
 
 
-    first_line_image_id=$(echo "$docker_image_details" | awk 'NR==2 {print $3}')
-    second_line_image_id=$(echo "$docker_image_details" | awk 'NR==3 {print $3}')
-    echo "First Line Image ID: $first_line_image_id"
-    echo "Second Line Image ID: $second_line_image_id"
+        first_line_image_id=$(echo "$docker_image_details" | awk 'NR==2 {print $3}')
+        second_line_image_id=$(echo "$docker_image_details" | awk 'NR==3 {print $3}')
+        echo "First Line Image ID: $first_line_image_id"
+        echo "Second Line Image ID: $second_line_image_id"
 
-  echo "Deleting Docker images..."
-  docker rmi "$first_line_image_id" "$second_line_image_id"
-"""
- }
+        echo "Deleting Docker images..."
+        docker rmi "$first_line_image_id" "$second_line_image_id"
+    """
+}
