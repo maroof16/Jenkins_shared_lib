670fd41a297cbe209d041278ea8066eac62e302d
maroof <maroofshaikh09@gmail.com>
2023-07-09 20:32:40 +0530

    push docker image on ECR

--- vars/dockerBuildECR.groovy
+++ vars/dockerBuildECR.groovy
@@ -1,6 +1,6 @@
 def call(String aws_account_id, String region, String ECR_repo_name){
     sh """
-        docker build ${ECR_repo_name} .
+        docker build -t ${ECR_repo_name} .
         docker tag ${ECR_repo_name}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ECR_repo_name}:latest
     """
 }
